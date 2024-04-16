07dec40251ca77c095e0a87e3bcae3544435da2b
B. Sai Bhaskar <bhamidipati.2022ug1016@iiitranchi.ac.in>
2024-04-17 02:15:29 +0530

    body-parser added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const { port } = require("./config/serverConfig.js");
 
 const setupAndStartServer = async () => {
   const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
   app.listen(port, () => console.log(`app listening on port ${port}!`));
 };
