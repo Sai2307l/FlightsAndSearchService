107d139e1f844155270c50ddcf0ffdb79e3dae23
B. Sai Bhaskar <bhamidipati.2022ug1016@iiitranchi.ac.in>
2024-05-19 09:47:47 +0530

    airplanes DB

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const { port } = require("./config/serverConfig.js");
 const ApiRoutes = require("./routes/index.js");
 
 const db = require("./models/index.js");
+const { Airplane } = require("./models/index.js");
+const { where, Model } = require("sequelize");
+// const { Airplane } = require("./models/airplane.js");
 
 const setupAndStartServer = async () => {
   const app = express();
